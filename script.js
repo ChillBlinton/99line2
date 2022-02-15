26938004a5e722aaf90507113c40e772da1152e9
CB <wtbatson@gmail.com>
2022-02-15 14:36:15 -0600

    init commit

---
+++ script.js
@@ -0,0 +1,30 @@
+let friends = [
+    "Chungus",
+    "Amongus",
+    "Fungus",
+    "bruhngus",
+    "Jeb"
+]
+
+function lineyLine(friends) {
+    let counter = 99;                       
+    let counterMinus = (counter-1)        
+    while (counter > 2) {
+        console.log(counter + " lines of code in the file, " + counter + " lines of code;" + friends + " strikes one out, clears it all out, " + counterMinus + " lines of code in the file.");
+        counter--;
+        counterMinus--;
+    }    
+    if (counter == 2) {
+        console.log(counter + " lines of code in the file, " + counter + " lines of code;" + friends + " strikes one out, clears it all out, " + counterMinus + " line of code in the file.");
+  }
+    console.log(counterMinus + " line of code in the file, " + counterMinus + " line of code; Go to the source, declare some more, 99 lines of code in the file. ")
+}
+
+for (let i = 0; i < friends.length; i++) {
+    const element = friends[i];
+    lineyLine(friends[i])
+    for (let d = 0; d < i.length; d++) {
+        const element = i[d];
+        
+    }
+}
